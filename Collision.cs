1462ef276982e8c289574fac52ae0e01ae536787
Adam Jacks <adamjacks1231@gmail.com>
2022-05-18 17:35:15 +0100

    Improved player controller

---
+++ Collision.cs
@@ -0,0 +1,38 @@
+﻿using BingingOfRandy.Enums;
+using BingingOfRandy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingingOfRandy
+{
+    public static class Collision
+    {
+        public static Colliders Check(int x, int y)
+        {
+            var player = Program.player;
+            var room = Program.rooms[player.mapX, player.mapY];
+
+            if (room.layout[y, x] == '#')
+            {
+                return Colliders.Wall;
+            }
+
+            if (room.layout[y, x] == 'O')
+            {
+                return Colliders.Wall;
+            }
+
+            if (player.x == x && player.y == y)
+            {
+                return Colliders.Player;
+            }
+
+            // check if coords match any enemy in current room
+
+            return Colliders.None;
+        }
+    }
+}
---
+++ Enums/Colliders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingingOfRandy.Enums
+{
+    public enum Colliders
+    {
+        Player,
+        Enemy,
+        Bullet,
+        Wall,
+        Hole,
+        None
+    }
+}
